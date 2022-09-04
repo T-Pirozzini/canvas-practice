53e3fe0c68d30d4df692ecea166cfc2bcf3e8b38
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-09-04 16:45:50 -0700

    add FlowFieldEffect class

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 window.onload = function () {
   const canvas = document.getElementById('canvas1');
   const ctx = canvas.getContext('2d');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  const flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
+}
+
+class FlowFieldEffect {
+  #ctx;
+  #width;
+  #height;
+  constructor(ctx, width, height) {
+    this.#ctx = ctx;
+    this.#ctx.strokeStyle = 'white';
+    this.#width = width;
+    this.#height = height;
+    console.log('effect loaded')
+    this.#draw(10, 10)
+  }
+  #draw(x, y) {
+    this.#ctx.beginPath();
+    this.#ctx.moveTo(x, y);
+    this.#ctx.lineTo(x + 100, y + 100);
+    this.#ctx.stroke();
+  }
 }
