09b88a087aa544e696589203d9d7ec0ef8b4e9c4
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-09-05 12:04:21 -0700

    add colorStop gradients

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ window.addEventListener('resize', function() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
-  flowField.animate();
+  flowField.animate(0);
 });
 
 const mouse = {
@@ -38,21 +38,49 @@ class FlowFieldEffect {
     this.#ctx.strokeStyle = 'white';
     this.#ctx.lineWidth = 5;
     this.#width = width;
-    this.#height = height;
-    this.angle = 0;      
+    this.#height = height;    
+    this.lastTime = 0;
+    this.interval = 1000/60; // 60 frames per second
+    this.timer = 0; 
+    this.cellSize = 15;
+    this.gradient;
+    this.#createGradient();
+    this.#ctx.strokeStyle = this.gradient;
   }
-  #draw(x, y) {
+  #createGradient() {
+    this.gradient = this.#ctx.createLinearGradient(0, 0, this.#width, this.#height);
+    this.gradient.addColorStop("0.1", "#ff5c33");
+    this.gradient.addColorStop("0.2", "#ff66b3");
+    this.gradient.addColorStop("0.4", "#ccccff");
+    this.gradient.addColorStop("0.6", "#b3ffff");
+    this.gradient.addColorStop("0.8", "#80ff80");
+    this.gradient.addColorStop("0.9", "#ffff33");
+
+  }
+  #drawLine(x, y) {
     const length = 300;
     this.#ctx.beginPath();
     this.#ctx.moveTo(x, y);
-    this.#ctx.lineTo(mouse.x, mouse.y);
+    this.#ctx.lineTo(x + 25, y + 10);
     this.#ctx.stroke();
   }
-  animate() {
-    this.angle += 0.1;
-    this.#ctx.clearRect(0, 0, this.#width, this.#height);
-    this.#draw(this.#width/2, this.#height/2);    
-    // console.log('animating');
+  animate(timeStamp) {
+    const deltaTime = timeStamp - this.lastTime;
+    this.lastTime = timeStamp;
+    // if (this.timer > this.interval) {      
+      this.#ctx.clearRect(0, 0, this.#width, this.#height);
+
+      for (let y = 0; y < this.#height; y += this.cellSize) {
+        for (let x = 0; x < this.#width; x += this.cellSize) {
+          this.#drawLine(x, y);
+        }
+      // } 
+
+      this.timer = 0;
+    // } else {
+      this.timer += deltaTime;
+    }
+    
     flowFieldAnimation = requestAnimationFrame(this.animate.bind(this));
   }
 }
